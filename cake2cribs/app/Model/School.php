c009434b447bb096deb4c63462f20874154a5c47
Tim <tim@Tim.(none)>
2013-03-16 15:00:54 -0400

    added distance calculation to only pull markers within specified radius of selected school

--- cake2cribs/app/Model/School.php
+++ cake2cribs/app/Model/School.php
@@ -23,6 +23,19 @@ class School extends AppModel {
 	{
 		return $this->find('all');
 	}
+
+	public function getTargetLatLong($school_id)
+	{
+		$lat_long = $this->find('first', array(
+			'conditions' => array('School.school_id' => $school_id),
+			'fields' => 	array('center_lat', 'center_long')));
+		CakeLog::write("School", print_r($lat_long, true));
+		$return_val = array(
+		'latitude' => $lat_long['School']['center_lat'],
+		'longitude' => $lat_long['School']['center_long']);
+
+		return $return_val; 
+	}
 }
 
 ?> 
