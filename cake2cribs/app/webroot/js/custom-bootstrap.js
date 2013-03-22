a10c9884a814f588576c33fce40ed5989e5bb91e
Evan Dancer <edance4@gmail.com>
2013-03-22 00:51:48 -0400

    changed js for bootstrap popover to hide all the tooltips and only one can be displayed at once. Also added Whenever button to datepicker

--- cake2cribs/app/webroot/js/custom-bootstrap.js
+++ cake2cribs/app/webroot/js/custom-bootstrap.js
@@ -1200,10 +1200,75 @@
       return this.getTitle() || this.getContent()
     }
 
+  , show: function () {
+      var $tip
+        , pos
+        , actualWidth
+        , actualHeight
+        , placement
+        , tp
+        , e = $.Event('show')
+
+      var that = this;
+      $(document).on('mousedown', function(ev){
+        if ($(ev.target).closest('.popover').length == 0 && !$(ev.target).closest('.popover-btn').is(that.$element)) {
+          that.hide();
+        }
+      });
+
+      $('.tooltip').hide()  
+
+      if (this.hasContent() && this.enabled) {
+        this.$element.trigger(e)
+        if (e.isDefaultPrevented()) return
+        $tip = this.tip()
+        this.setContent()
+
+        if (this.options.animation) {
+          $tip.addClass('fade')
+        }
+
+        placement = typeof this.options.placement == 'function' ?
+          this.options.placement.call(this, $tip[0], this.$element[0]) :
+          this.options.placement
+
+        $tip
+          .detach()
+          .css({ top: 0, left: 0, display: 'block' })
+
+        this.options.container ? $tip.appendTo(this.options.container) : $tip.insertAfter(this.$element)
+
+        pos = this.getPosition()
+
+        actualWidth = $tip[0].offsetWidth
+        actualHeight = $tip[0].offsetHeight
+
+        switch (placement) {
+          case 'bottom':
+            tp = {top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2}
+            break
+          case 'top':
+            tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2}
+            break
+          case 'left':
+            tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth}
+            break
+          case 'right':
+            tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width}
+            break
+        }
+
+        this.applyPlacement(tp, placement)
+        this.$element.trigger('shown')
+      }
+    }    
+
   , hide: function () {
       var that = this
         , $tip = this.tip()
         , e = $.Event('hide')
+      
+      $(document).off('mousedown');
 
       this.$element.trigger(e)
       if (e.isDefaultPrevented()) return
