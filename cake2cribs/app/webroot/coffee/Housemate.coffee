81a0ee1dc7672ebd671a4bc18ca11730df00ad5b
Tim <tim@Tim.(none)>
2013-03-23 17:15:13 -0400

    added model and sql files for student and gender type tables. Added Housemate coffee and js file.

---
+++ cake2cribs/app/webroot/coffee/Housemate.coffee
@@ -0,0 +1,2 @@
+class A2Cribs.Housemate
+	constructor: (@SubletId, @Enrolled, @Major, @Seeking, @GradType, @Gender) ->
---
+++ cake2cribs/app/webroot/js/src/Housemate.js
@@ -0,0 +1,18 @@
+(function() {
+
+  A2Cribs.Housemate = (function() {
+
+    function Housemate(SubletId, Enrolled, Major, Seeking, GradType, Gender) {
+      this.SubletId = SubletId;
+      this.Enrolled = Enrolled;
+      this.Major = Major;
+      this.Seeking = Seeking;
+      this.GradType = GradType;
+      this.Gender = Gender;
+    }
+
+    return Housemate;
+
+  })();
+
+}).call(this);
