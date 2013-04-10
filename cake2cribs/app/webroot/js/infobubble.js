8ae9f78bb5327bd0ef98194cf490261202d522d2
Mike Stratman <michaelstratman@gmail.com>
2013-04-09 20:44:33 -0400

    resolved #18 infobubble.js had a broken relative link to the close-button img resource

--- cake2cribs/app/webroot/js/infobubble.js
+++ cake2cribs/app/webroot/js/infobubble.js
@@ -215,7 +215,7 @@ InfoBubble.prototype.buildDom_ = function() {
   close.style['border'] = 0;
   close.style['zIndex'] = this.baseZIndex_ + 1;
   close.style['cursor'] = 'pointer';
-  close.src = './img/close_button.png';
+  close.src = '/img/close_button.png';
 
   var that = this;
   google.maps.event.addDomListener(close, 'click', function() {
