43a730039e8d2059fd89eb863a506af515acd5f9
Tim <tim@Tim.(none)>
2013-04-16 11:54:32 -0400

    added missing PropertyManagement js file

---
+++ cake2cribs/app/webroot/js/src/PropertyManagement.js
@@ -0,0 +1,24 @@
+(function() {
+
+  A2Cribs.PropertyManagement = (function() {
+
+    function PropertyManagement() {}
+
+    PropertyManagement.removeSublet = function(id) {
+      var _this = this;
+      return alertify.confirm("Are you sure you want to delete this property? This can't be undone.", function(e) {
+        var url;
+        if (e) {
+          url = myBaseUrl + ("sublets/remove/" + id);
+          return window.location.href = url;
+        } else {
+
+        }
+      });
+    };
+
+    return PropertyManagement;
+
+  })();
+
+}).call(this);
