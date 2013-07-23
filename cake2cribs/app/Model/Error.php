53b5442bb2941e4eb9e5f770d695258e5c64acf0
Tim <tim@Tim.(none)>
2013-07-23 15:57:52 -0400

    added missing error class for dev site

---
+++ cake2cribs/app/Model/Error.php
@@ -0,0 +1,24 @@
+<?php 
+
+class Error extends AppModel {
+	public $name = 'Error';
+	public $primaryKey = 'error_id';
+
+	public $validate = array(
+		'error_id' => 'numeric',
+		'user_id' => 'numeric',
+		'error_code' => 'numeric'
+	);	
+
+	public function AddError($user_id, $error_code, $debug_info)
+	{
+		$error = array(
+			'Error'=>array(
+				'user_id' => $user_id,
+				'error_code' => $error_code,
+				'debug_info' => $debug_info
+		));
+		$this->save($error);
+	}
+}
+?>
