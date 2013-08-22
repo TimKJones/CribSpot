5f192a69e9478e9f339be764c52b60fd86052755
Michael Stratman <michaelstratman@gmail.com>
2013-08-21 20:19:04 -0700

    Listings:FindNear functiona added, sql functions for all my tables are now under revision as well, started putting in a modification to ListingController::getListing that would in teh case of the user being a newspaper admin; fetch all the listings withiin a certain radius of the center of the university they are associated with. These listings are then passed along in the same fashion as the getListingsForLoggedinuser return listings go

---
+++ cake2cribs/app/Model/sql/FeaturedListings.sql
@@ -0,0 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 22, 2013 at 05:14 AM
+-- Server version: 5.5.25
+-- PHP Version: 5.4.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `cake2cribs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `featured_listings`
+--
+
+CREATE TABLE `featured_listings` (
+  `int` int(11) NOT NULL,
+  `listing_id` int(11) NOT NULL,
+  `university_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ cake2cribs/app/Model/sql/PendingOrders.sql
@@ -0,0 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 22, 2013 at 05:12 AM
+-- Server version: 5.5.25
+-- PHP Version: 5.4.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `cake2cribs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pending_orders`
+--
+
+CREATE TABLE `pending_orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `orderItems` text NOT NULL,
+  `order_type` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
