7ca265e8c6ff0f317e915826cbab0a869c32c2e4
Tim <tim@Tim.(none)>
2013-08-27 11:50:21 -0400

    added database backup shell

--- cake2cribs/app/Console/Command/HelloShell.php
+++ cake2cribs/app/Console/Command/HelloShell.php
@@ -8,8 +8,12 @@ class HelloShell extends AppShell
         $this->out('Hello world.');
     }
 
-    public function db_backup() {
-        shell_exec("mysqldump cake2cribs -u root -proot > ~/dumps/dump.sql");
+    public function db_backup1() {
+        shell_exec("mysqldump cribspot -u root -plancPA*travMInj > ~/CribSpot/cake2cribs/app/webroot/dumps/dump1.sql");
+    }
+
+    public function db_backup2() {
+        shell_exec("mysqldump cribspot -u root -plancPA*travMInj > ~/CribSpot/cake2cribs/app/webroot/dumps/dump2.sql");
     }
 }
 
