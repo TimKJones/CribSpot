252f4700e77c2d7391722a7dcf6226446d9c5421
Tim <tim@Tim.(none)>
2013-08-29 20:47:06 -0400

    changed name of db to backup

--- cake2cribs/app/Console/Command/DBBackupShell.php
+++ cake2cribs/app/Console/Command/DBBackupShell.php
@@ -5,11 +5,11 @@ class DBBackupShell extends AppShell
 	public $uses = array('Listing');
 
     public function db_backup1() {
-        shell_exec("mysqldump cribspot -u root -plancPA*travMInj > ~/CribSpot/cake2cribs/app/webroot/dumps/dump1.sql");
+        shell_exec("mysqldump cake2cribs -u root -plancPA*travMInj > ~/CribSpot/cake2cribs/app/webroot/dumps/dump1.sql");
     }
 
     public function db_backup2() {
-        shell_exec("mysqldump cribspot -u root -plancPA*travMInj > ~/CribSpot/cake2cribs/app/webroot/dumps/dump2.sql");
+        shell_exec("mysqldump cake2cribs -u root -plancPA*travMInj > ~/CribSpot/cake2cribs/app/webroot/dumps/dump2.sql");
     }
 }
 
