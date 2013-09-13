2357e4dd25d2ba4d24d73732eb140776f7363df9
Tim <tim@Tim.(none)>
2013-09-13 14:27:28 -0400

    added log cleanup....still need to add this to crontab on prod

--- cake2cribs/app/Console/Command/BackupShell.php
+++ cake2cribs/app/Console/Command/BackupShell.php
@@ -11,6 +11,21 @@ class BackupShell extends AppShell
     public function db_backup2() {
         shell_exec("mysqldump cake2cribs -u root -plancPA*travMInj > ~/CribSpot/cake2cribs/app/webroot/dumps/dump2.sql");
     }
+
+    /*
+	Deletes all files in /tmp/logs
+	Should be run a few times a week
+    */
+    public function logs_cleanup() {
+    	$dir = TMP . '/logs';
+    	foreach (scandir($dir) as $item) {
+	        if ($item == '.' || $item == '..') 
+	        	continue;
+
+	        $old = $dir.'/'.$item;
+	        unlink($old);
+	    }
+    }
 }
 
 ?>
