03689c21290f44f51c2d85d708df1ff1c862391e
Tim <tim@Tim.(none)>
2013-09-16 22:48:12 -0400

    added daily user report

--- cake2cribs/app/Console/Command/EmailShell.php
+++ cake2cribs/app/Console/Command/EmailShell.php
@@ -1,7 +1,7 @@
 <?php 
 
 class EmailShell extends AppShell{
-    public $uses = array('Error');
+    public $uses = array('Error', 'User');
 
     // Fetch all the listings that will be featured over the next 3 days
     // and email them out to people.
@@ -23,57 +23,28 @@ class EmailShell extends AppShell{
         $year = date('Y');
         $subject = "Error Report for " . $month.' '.$day.', '.$year;
         $recipient = Configure::read('ERROR_REPORT_RECIPIENT');
-        $this->_emailUser($recipient, $subject, "errors", $template_data);
+        $this->_emailUser($recipient, $subject, "errors", $template_data);  
+    }
 
+    public function email_daily_user_report(){
+        // Set timezone
+        date_default_timezone_set('UTC');
+        // Start date
+        $today = date('Y-m-d');
+        $yesterday = date('Y-m-d', strtotime('-1 day', strtotime($today)));
+        $users = $this->User->find('all', array(
+            'conditions' => array('User.created >=' => $yesterday)
+        ));
 
-/*        $listings = array();
-        for($i = 0; $i < $num_days; ++$i){
-            $listings[$date] = array();
-            $listing_ids = $this->FeaturedListing->getByDate($date);
-            $featuredListings = $this->Listing->find('all', array(
-                'conditions' => array(
-                    'Listing.listing_id' => $listing_ids
-                )
-            ));
-            // Go through and add the relevant data as a new array to the listings array
-            foreach ($featuredListings as $key => $fl) {
-                $fldata = array(
-                    'listing_id' => $fl['Listing']['listing_id'],
-                    'address' => $fl['Marker']['street_address'],
-                    'beds'=>$fl['Rental']['beds'],
-                    'baths'=>$fl['Rental']['baths'],
-                    'rent'=>$fl['Rental']['rent'],
-                    'highlights'=>$fl['Rental']['highlights'],
-                    'description'=>$fl['Rental']['description'],
-                    'contact_email'=>$fl['Rental']['contact_email'],
-                    'contact_phone'=>$fl['Rental']['contact_phone'],
-                    'listing_url'=>'www.cribspot.com/listing/' . $fl['Listing']['listing_id']   
-                    );
-                $fldata['primary_image_url'] = '';
-                if (array_key_exists('Image', $fl)){
-                    foreach ($fl['Image'] as $image){
-                        if (array_key_exists('is_primary', $image) && intval($image['is_primary']) === 1)
-                            $fldata['primary_image_url'] = 'www.cribspot.com/' . $image['image_path'];
-                    }
-                }
-
-                if (!empty($fl['Marker']['alternate_name']))
-                    $fldata['address'] = $fl['Marker']['alternate_name'];
-
-                array_push($listings[$date],$fldata);
-            }
-
-            $date = date ("Y-m-d", strtotime("+1 day", strtotime($date)));
-        }      
-
-        $template_data = array("listings"=>$listings);
+        $template_data = array("users"=>$users);
         $month = date('F');
         $day = date('j') - 1;
         $year = date('Y');
-        $subject = "Featured Listings Report for " . $month.' '.$day.', '.$year;
-        $recipient = Configure::read('FEATURED_LISTINGS_REPORT_RECIPIENT');
-        $this->_emailUser($recipient, $subject, "featured_listings", $template_data);
-        */
+        $subject = "New User Report for " . $month.' '.$day.', '.$year;
+        $recipients = Configure::read('USER_REPORT_RECIPIENTS');
+        foreach ($recipients as $recipient){
+            $this->_emailUser($recipient, $subject, "users", $template_data);  
+        }
     }
 }
 
---
+++ cake2cribs/app/View/Emails/html/users.ctp
@@ -0,0 +1,26 @@
+<style>
+    .ui-helper-center {
+        text-align: center;
+        padding: 5px;
+    }
+</style>
+
+<div id = 'users-wrapper'>
+<?php
+foreach ($users as $user){
+    echo "<b>User ID: " . $user['User']['id'] . "</b><br/>";
+    echo "Email: " . $user['User']['email'] . "<br/>";
+    if ($user['User']['user_type'] == 0){
+    	echo "User Type: Student<br/>";
+    	echo "First Name: " . $user['User']['first_name'] . "<br/>";
+    	echo "Last Name: " . $user['User']['last_name'] . "<br/>";
+    }
+    else if ($user['User']['user_type'] == 1){
+    	echo "User Type: Property Manager<br/>";
+    	echo "Company Name: " . $user['User']['company_name'] . "<br/>";
+    	echo "Phone Number: " . $user['User']['phone'] . "<br/>";
+    }
+    echo "---------------<br/>";
+}
+?>
+</div>
