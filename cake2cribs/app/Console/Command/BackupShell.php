b617dcc034a3b703e93ce45df08be5bf442c53dd
Tim <tim@Tim.(none)>
2013-09-26 17:45:54 -0400

    shifting backups to s3

--- cake2cribs/app/Console/Command/BackupShell.php
+++ cake2cribs/app/Console/Command/BackupShell.php
@@ -5,7 +5,13 @@ class BackupShell extends AppShell
 	public $uses = array('Listing');
 
     public function db_backup1() {
-        shell_exec("mysqldump cake2cribs -u root -plancPA*travMInj > ~/CribSpot/cake2cribs/app/webroot/dumps/dump1.sql");
+        $localFilePath = "~/CribSpot/cake2cribs/app/webroot/dumps/dump1.sql";
+        $s3_path = "daily_backups/dump1.sql";
+        shell_exec("mysqldump cake2cribs -u root -plancPA*travMInj > " . $localFilePath);
+        /* Upload file to s3 */
+        $this->_uploadFileToS3($localFilePath, $s3_path);
+        /* delete local file */
+        unlink($localFilePath);
     }
 
     public function db_backup2() {
@@ -37,6 +43,18 @@ class BackupShell extends AppShell
         $year = date('Y');
         $filename = $month.'_'.$day.'_'.$year.'_snapshot.sql';
         shell_exec("mysqldump cake2cribs -u root -plancPA*travMInj > ~/CribSpot/cake2cribs/app/webroot/dumps/".$filename);
+    }
+
+    private function _uploadFileToS3($localFilePath, $s3_path)
+    {
+        App::import('Vendor', 'AmazonS3/S3');
+        $accessKey = Configure::read('S3_ACCESS_KEY');
+        $secretKey = Configure::read('S3_SECRET_KEY');
+        $backupBucket = Configure::read('S3_BACKUP_BUCKET');
+        $s3 = new S3($accessKey, $secretKey);
+        if (!$s3->putObjectFile($localFilePath, $backupBucket, $s3_path, S3::ACL_PUBLIC_READ)) {
+            CakeLog::write('FAILED_BACKUP', $localFilePath . '; ' . $backupBucket . '; ' . $s3_path);
+        }
     }
 }
 
