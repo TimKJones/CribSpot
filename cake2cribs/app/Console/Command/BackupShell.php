3a8c01714c0a922ff73c81e15047d01063900662
Tim <tim@Tim.(none)>
2013-10-16 15:24:47 -0400

    now have dumps backed up to s3

--- cake2cribs/app/Console/Command/BackupShell.php
+++ cake2cribs/app/Console/Command/BackupShell.php
@@ -5,18 +5,27 @@ class BackupShell extends AppShell
 	public $uses = array('Listing');
 
     public function db_backup1() {
-        $localFilePath = "~/CribSpot/cake2cribs/app/webroot/dumps/dump1.sql";
+        $localFilePath = "/home/tim/CribSpot/cake2cribs/app/webroot/dumps/dump1.sql";
         $s3_path = "daily_backups/dump1.sql";
-        shell_exec("mysqldump cake2cribs -u root -proot > ~/CribSpot/cake2cribs/app/webroot/dumps/dump1.sql");
-        chmod ($localFilePath, 0777);
+        shell_exec("mysqldump cake2cribs -u root -plancPA*travMIn > ~/CribSpot/cake2cribs/app/webroot/dumps/dump1.sql");
+
         /* Upload file to s3 */
-        $this->_uploadFileToS3($localFilePath, $s3_path);
+        shell_exec("~/Downloads/s3cmd-1.5.0-alpha1/s3cmd sync dump1.sql s3://cribspot-backup/daily_backups");
+
         /* delete local file */
         unlink($localFilePath);
     }
 
-    public function db_backup2() {
+    public function db_backup2() {      
+        $localFilePath = "/home/tim/CribSpot/cake2cribs/app/webroot/dumps/dump1.sql";
+        $s3_path = "daily_backups/dump2.sql";
         shell_exec("mysqldump cake2cribs -u root -plancPA*travMInj > ~/CribSpot/cake2cribs/app/webroot/dumps/dump2.sql");
+
+        /* Upload file to s3 */
+        shell_exec("~/Downloads/s3cmd-1.5.0-alpha1/s3cmd sync dump2.sql s3://cribspot-backup/daily_backups");
+
+        /* delete local file */
+        unlink($localFilePath);
     }
 
     /*
@@ -53,7 +62,7 @@ class BackupShell extends AppShell
         $secretKey = Configure::read('S3_SECRET_KEY');
         $backupBucket = Configure::read('S3_BACKUP_BUCKET');
         $s3 = new S3($accessKey, $secretKey);
-        if (!$s3->putObjectFile($localFilePath, $backupBucket, $s3_path, S3::ACL_PUBLIC_READ)) {
+        if (!$s3->putObjectFile($localFilePath, $backupBucket, $s3_path, S3::ACL_PRIVATE)) {
             CakeLog::write('FAILED_BACKUP', $localFilePath . '; ' . $backupBucket . '; ' . $s3_path);
         }
     }
