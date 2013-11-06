68781fee7da2dae92b2b518b272137d00eb573e0
unknown <karnvish@umich.edu>
2013-11-06 17:34:20 -0500

    parking model

--- cake2cribs/app/Model/Parking.php
+++ cake2cribs/app/Model/Parking.php
@@ -1,14 +1,48 @@
-<?php 	
+ <?php
 
-class Parking extends AppModel {
-	public $name = 'Parking';
-	public $primaryKey = 'parking_id';
-	/*public $belongsTo = array('');*/
+ class Parking extends AppModel{
 
-	public $validate = array(
-		'parking_id' => 'numeric',
-		'listing_id' => 'numeric'
-	);
-}
+ 	public $name = 'spot';
+ 	public $primaryKey = 'spot_id';
+ 	public $belongsTo = array(
+ 		'Listing' => array(
+ 			'className' => 'Listing',
+ 			'foreignKey' => 'listing_id'
+ 			)
+		);	
 
-?>
+ 	public $actsAs = array('Containable');
+
+ 	public $validate  array(
+ 		'spot_id' => 'numeric',
+ 		'listing_id' => 'numeric',
+ 		'price' => 'numeric',
+
+ 		'space_count' => array(  // Not required, it will defualt to one
+			'numeric' => array(
+				'rule' => 'numeric',
+				'required' => false
+			)
+		), 
+ 		
+ 		'start_date' => array(
+			'date' => array(
+				'rule' => array('date', 'ymd'),
+				'required' => true
+			),
+		'end_date' => array(
+			'date' => array(
+				'rule' => array('date', 'ymd'),
+				'required' => true
+			),
+		'description' => array(
+				'between' => array(
+				'rule' => array('between',0,1000),
+				'message' => 'Must be less than 1000 characters'
+			)
+		),
+
+		'is_complete' => 'boolean',
+		'created' => 'datetime',
+		'modified' => 'datetime'
+	};
